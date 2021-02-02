f4e3985ef722036108d92a9d85f18ce3f4ba7de6
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-02-02 17:20:58 +0300

    Create URL.php

---
+++ src/URL.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Bermuda\Utils;
+
+/**
+ * Class URL
+ * @package Bermuda\Utils
+ */
+final class URL
+{
+    public function __construct()
+    {
+        throw new \RuntimeException(__CLASS__ . ' is not instantiable');
+    }
+
+    /**
+     * @param array $params
+     * @return string
+     */
+    public static function build(array $params = []): string
+    {
+        $url = ($params['schema'] ?? self::schema()) . '://';
+
+        if (!empty($params['user']))
+        {
+            $url .= $params['user'] . ':' . $params['pass'] . '@';
+        }
+
+        $url .= ($params['host'] ?? $_SERVER['HTTP_HOST']);
+
+        if (!empty($params['port']))
+        {
+            $url .= ':' . $params['port'];
+        }
+
+        $url .= '/';
+
+        if (!empty($params['path']))
+        {
+            $url .= '?' . http_build_query((array) $params['path']);
+        }
+
+        elseif (!empty($_SERVER['QUERY_STRING']))
+        {
+            $url .= '?' . $_SERVER['QUERY_STRING'];
+        }
+
+        if (!empty($params['anchor']))
+        {
+            $url .= '#' . $params['anchor'];
+        }
+
+        return $url;
+    }
+
+    /**
+     * @return string
+     */
+    private static function schema(): string
+    {
+        return $_SERVER['SERVER_PORT'] === '443'
+        || !empty($_SERVER['HTTPS']) ? 'https' : 'http';
+    }
+}
