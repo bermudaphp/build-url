bf7a396c38fab7054206f81fb2945390c5113bc6
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-08-30 22:03:08 +0200

    Update MimeType.php

--- src/MimeType.php
+++ src/MimeType.php
@@ -4,16 +4,30 @@ namespace Bermuda\Utils;
 
 final class MimeType
 {
-    /**
-     * image mime types
-     */
-    public const gif = 'image/gif';
-    public const jpeg = 'image/jpeg';
-    public const pjpeg = 'image/pjpeg';
-    public const png = 'image/png';
-    public const svg = 'image/svg+xml';
-    public const tiff = 'image/tiff';
+    public const gif                = 'image/gif';
+    public const jpeg               = 'image/jpeg';
+    public const pjpeg              = 'image/pjpeg';
+    public const png                = 'image/png';
+    public const svg                = 'image/svg+xml';
+    public const tiff               = 'image/tiff';
     public const vnd_microsoft_icon = 'vnd.microsoft.icon';
-    public const vnd_wap_wbmp = 'image/vnd.wap.wbmp';
-    public const webp = 'image/webp';
+    public const vnd_wap_wbmp       = 'image/vnd.wap.wbmp';
+    public const webp               = 'image/webp';
+    
+    public const html               = 'text/html';
+    public const plain              = 'text/plain';
+    public const css                = 'text/css';
+    public const csv                = 'text/csv';
+    public const javaScript         = 'javascript';
+    public const php                = 'text/php';
+    public const xml                = 'text/xml';
+    public const markdown           = 'text/markdown';
+    public const cacheManifest      = 'text/cache-manifest';
+    public const cmd                = 'text/cmd';
+    
+    public const atomXml            = 'application/atom+xml';
+    public const EDIX12             = 'application/EDI-X12';
+    public const EDIFACT            = 'application/EDIFACT';
+    public const applicationJson    = 'application/json';
+    public const applicationJavaScript = 'application/javascript';
 }
