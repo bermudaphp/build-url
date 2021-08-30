4aa651f2ea6488e0ef0cfc0f68b5fce06cf12aa5
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-08-30 21:50:35 +0200

    Create ContentType.php

---
+++ src/ContentType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bermuda\Utils;
+
+final class ContentType
+{
+    public const html = 'text/html';
+    public const text = 'text/plain';
+    public const json = 'application/json';
+}
