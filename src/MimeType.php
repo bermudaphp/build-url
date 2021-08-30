b1c47131b86aa96d341509d9ed1d4272dd5ac1f9
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-08-30 21:23:38 +0200

    Create MimeType.php

---
+++ src/MimeType.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Bermuda\Utils;
+
+final class MimeType
+{
+    /**
+     * image mime types
+     */
+    public const gif = 'image/gif';
+    public const jpeg = 'image/jpeg';
+    public const pjpeg = 'image/pjpeg';
+    public const png = 'image/png';
+    public const svg = 'image/svg+xml';
+    public const tiff = 'image/tiff';
+    public const vnd_microsoft_icon = 'vnd.microsoft.icon';
+    public const vnd_wap_wbmp = 'image/vnd.wap.wbmp';
+    public const webp = 'image/webp';
+}
