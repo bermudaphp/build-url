8ff97184d15626d05a64819a70a67cc6653703ae
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-08-30 21:44:17 +0200

    Create Header.php

---
+++ src/Header.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Bermuda\Utils;
+
+final class Header
+{
+    public const acceptCharset      = 'Accept-Charset';
+    public const acceptEncoding     = 'Accept-Encoding';
+    public const acceptLanguage     = 'Accept-Language';
+    public const acceptRanges       = 'Accept-Ranges';
+    public const age                = 'Age';
+    public const allow              = 'Allow';
+    public const alternates         = 'Alternates';
+    public const authorization      = 'Authorization';
+    public const cacheControl       = 'Cache-Control';
+    public const connection         = 'Connection';
+    public const contentDisposition = 'Content-Disposition';
+    public const contentEncoding    = 'Content-Encoding';
+    public const contentLanguage    = 'Content-Language';
+    public const contentLength      = 'Content-Length';
+    public const contentLocation    = 'Content-Location';
+    public const contentMD5         = 'Content-MD5';
+    public const contentRange       = 'Content-Range';
+    public const contentType        = 'Content-Type';
+    public const contentVersion     = 'Content-Version';
+    public const date               = 'Date';
+    public const derivedFrom        = 'Derived-From';
+    public const eTag               = 'ETag';
+    public const expect             = 'Expect';
+    public const expires            = 'Expires';
+    public const from               = 'From';
+    public const host               = 'Host';
+    public const ifMatch            = 'If-Match';
+    public const ifModifiedSince    = 'If-Modified-Since';
+    public const ifNoneMatch        = 'If-None-Match';
+    public const ifRange            = 'If-Range';
+    public const ifUnmodifiedSince  = 'If-Unmodified-Since';
+    public const lastModified       = 'Last-Modified';
+    public const link               = 'Link';
+    public const location           = 'Location';
+    public const maxForwards        = 'Max-Forwards';
+    public const mimeVersion        = 'MIME-Version';
+    public const pragma             = 'Pragma';
+    public const proxyAuthenticate  = 'Proxy-Authenticate';
+    public const proxyAuthorization = 'Proxy-Authorization';
+    public const public             = 'Public';
+    public const range              = 'Range';
+    public const referer            = 'Referer';
+}
