2ae4f1e71c3436d1909889809bc9509d3c2785ce
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-12 13:25:58 +0200

    Update Math.php

--- src/math/Math.php
+++ src/math/Math.php
@@ -4,12 +4,12 @@ namespace Bermuda\Utils\Math;
 
 final class Math
 {
-    public static function percent(int|flot $percent, int|float $of): int|float
+    public static function percent(int|float $percent, int|float $of): int|float
     {
         return $of/100 * $percent;
     }
   
-    public static function percentageOf(int|flot $number, int|float $of): int|float
+    public static function percentageOf(int|float $number, int|float $of): int|float
     {
         return $number/$of * 100 * 100;
     }
