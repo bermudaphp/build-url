91c775e96f49b35c7d39892bd8c10dcfcc4e6ade
Andrey Shelamkoff <inbox.shelamkoff@gmail.com>
2021-09-12 13:32:02 +0200

    Update Math.php

--- src/math/Math.php
+++ src/math/Math.php
@@ -9,8 +9,8 @@ final class Math
         return $of/100 * $percent;
     }
   
-    public static function percentageOf(int|float $number, int|float $of): int|float
+    public static function percentageOf(int|float $number, int|float $of, int $precision = 2): int|float
     {
-        return $number/$of * 100 * 100;
+        return round($number/$of * 100 * 100, $precision);
     }
 }
